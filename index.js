707dd6ea96e906a67a9744b28f2b4a7098ca4265
David L <159317073+daviscrib@users.noreply.github.com>
2024-03-18 22:26:38 +0100

    add background-color and margin

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const theme = document.querySelector(".theme");
 const body = document.querySelector("body");
 const photo = document.querySelector(".photo");
+const backgroundHeader = document.querySelector(".backgroundHeader");
 
 theme.addEventListener("click", () => {
-  body.classList.toggle("dark")
-  photo.classList.toggle("dark2")
+  body.classList.toggle("dark");
+  photo.classList.toggle("dark2");
+  backgroundHeader.classList.toggle("dark3");
 })
+
+function myFunction() {
+    var logo = document.createElement("IMG");
+    logo.setAttribute("src", "https://assets.codepen.io/6093409/sprocket.svg");
+    document.body.appendChild(logo);
+  }
