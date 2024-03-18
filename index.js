9eec6b1ba835fdf7e15daac3f9a3d7688c1f5460
David L <159317073+daviscrib@users.noreply.github.com>
2024-03-18 18:06:38 +0100

    add grayscale

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const theme = document.querySelector(".theme");
 const body = document.querySelector("body");
+const photo = document.querySelector(".photo");
 
 theme.addEventListener("click", () => {
-    body.classList.toggle("dark")
+  body.classList.toggle("dark")
+  photo.classList.toggle("dark2")
 })
